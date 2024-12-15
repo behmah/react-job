56677ef2ec301341f618f3ea099a8ba8e168528a
behmah <b2000yasini@gmail.com>
2024-12-15 23:21:32 +0430

    update agin

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import jsonServer from 'json-server';
+import path from 'path';  
+
 const server = jsonServer.create();
-const router = jsonServer.router('jobs.json'); // Replace with your JSON file
+const router = jsonServer.router(path.resolve('src/jobs.json'));  // Adjust the path if needed
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
