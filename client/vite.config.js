7b4929ccb81707e285eae1de3067bca637d7b832
behmah <b2000yasini@gmail.com>
2024-12-16 09:57:18 +0430

    relace local to live url server

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://react-job-anz2.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
